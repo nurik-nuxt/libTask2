9b4c2ba29299173e53f91eddae2243f3b2578643
nurtuganuly <nurtuganulyn@gmail.com>
2021-10-25 23:53:16 +0600

    build:go.mod

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/nurik-nuxt/libTask2
+
+go 1.16
+
+require (
+	github.com/aidarkhanov/nanoid/v2 v2.0.5
+)
---
+++ lib.go
@@ -0,0 +1,32 @@
+package libTask2
+
+import (
+	"errors"
+	"github.com/aidarkhanov/nanoid/v2"
+	"math"
+	"unicode"
+)
+
+func ChangeRegistCharacter(character rune) rune {
+	if unicode.IsLower(character) {
+		return unicode.ToUpper(character)
+	} else {
+		return unicode.ToLower(character)
+	}
+}
+
+func QuadraticRoot(a, b, c float64) (float64, float64, error) {
+	d := b*b - 4*a*c
+	if d < 0 {
+		return -1, -1, errors.New("discriminant is lower than zero")
+	}
+	d = math.Sqrt(d)
+	r1 := (-b + d) / (2 * a)
+	r2 := (-b - d) / (2 * a)
+	return r1, r2, nil
+}
+
+func GenerateUUID() string {
+	uuid, _ := nanoid.New()
+	return uuid
+}
